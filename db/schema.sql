b97a3803864403a43686957bc7ade0eff9826db1
Sean Conrad <sean.w.conrad@gmail.com>
2022-09-11 15:44:14 -0400

    added data to seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,25 +4,30 @@ create database employeetracker_db;
 use employeetracker_db;
 
 create table department (
-    id int not null auto_increment,
-    name varchar(30) not null,
-    primary key (id)
+    id int not null auto_increment primary key,
+    name_of_department varchar(30)  not null
+    
+    
 );
 
-create table role (
-    id int not null auto_increment,
+create table roles (
+    id int not null auto_increment primary key,
     title varchar(30) not null,
     salary decimal(8,2) not null,
     department_id int not null,
-    primary key (id)
+    foreign key (department_id)
+    references department(id)
+    
 );
 
 create table employee (
-    id int not null auto_increment,
+    id int not null auto_increment primary key,
     first_name varchar(30) not null,
     last_name varchar(30) not null,
     role_id int not null,
     manager_id int null,
-    primary key (id)
+    foreign key (role_id)
+    references roles(department_id)
        
+    
 );
---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+insert into department (name_of_department)
+values ("Sales"), ("Operations"), ("Marketing"), ("Engineering"), ("Software"), ("Human Resources");
+
+insert into roles (title, salary, department_id)
+values ("Sales Manager", 75000.00, 1),
+        ("Operations Manager", 70000.00, 2),
+        ("Marketing lead", 45000.00, 3),
+        ("Engineering Manager", 80000.00, 4),
+        ("Software Engineer", 90000.00, 5),
+        ("HR Manager", 70000.00, 6),
+        ("Operations agent", 25000.00, 2),
+        ("Sales Rep", 100000.00, 1),
+        ("Marketing rep", 70000.00, 3);
+
+insert into employee (first_name, last_name, role_id, manager_id)
+values ("Steve", "Smith", 1, null),
+        ("Susan", "Lewis", 2, null),
+        ("Phil", "Yang", 3,null),
+        ("Bill", "Smith", 4,null),
+        ("Stan", "Yonkers", 4,null),
+        ("Andy", "Angel", 5,null),
+        ("Lisa", "Worth", 6, null),
+        ("Hal", "Wolf", 1, null),
+        ("Sally", "Hess", 2, null),
+        ("Tanya", "Saunders", 3, null);
